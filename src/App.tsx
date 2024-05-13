ab26876671397626ee9ace4bfcedde1529fdf7f4
theprogrammedwords <rprasad43255@gmail.com>
2024-05-13 15:14:49 +0530

    Theming changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,45 +2,40 @@ import './App.css';
 import Theme from './theme/theme';
 import styled from 'styled-components';
 
-
 function App() {
   return (
-    <div className='App'>
-    <Theme>
-    <Container>
-      <Heading>Loan Form</Heading>
-    </Container>
-    <Body>
-     
-    </Body>
-    </Theme>
+    <div className="App">
+      <Theme>
+        <Container>
+          <Heading>Loan Form</Heading>
+        </Container>
+        <Body></Body>
+      </Theme>
     </div>
   );
 }
 
-
 const Container = styled.div`
   width: 100%;
   text-align: center;
-  border: ${props => `1px solid ${props.theme.colors.quaternary}`};
-  background-color: ${props => props.theme.colors.tertiary};
-  font-family: ${props => props.theme.fonts[0]};
+  border: ${(props) => `1px solid ${props.theme.colors.quaternary}`};
+  background-color: ${(props) => props.theme.colors.tertiary};
+  font-family: ${(props) => props.theme.fonts[0]};
 `;
 
 const Body = styled.div`
   margin: 40px;
   height: calc(100vh - 160px);
   text-align: center;
-  border: ${props => `2px solid ${props.theme.colors.tertiary}`};
-  background-color: ${props => props.theme.colors.quaternary};
-  font-family: ${props => props.theme.fonts[0]};
-  border-radius : 8px
+  border: ${(props) => `2px solid ${props.theme.colors.tertiary}`};
+  background-color: ${(props) => props.theme.colors.quaternary};
+  font-family: ${(props) => props.theme.fonts[0]};
+  border-radius: 8px;
 `;
 
 const Heading = styled.h1`
   color: ${({ theme: { colors } }) => colors.primary};
   font-size: ${({ theme: { fontSizes } }) => fontSizes.extra};
 `;
-
 
 export default App;
